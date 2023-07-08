2226a4d2ba2e78a5b4aa1e9a2924a4c26494d93f
AxelMoragaAlexandria <axelmoraga899@gmail.com>
2023-07-08 14:10:13 -0400

    I have added sun but it has some errors to be resolved

---
+++ src/FeatureSun.java
@@ -0,0 +1,12 @@
+public class FeatureSun {
+
+    String name;
+    String temp;
+    String size;
+
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,10 @@ public class Main {
         Omnax.Star="4";
 
 
-
+        FeatureSun AdamX=new FeatureSun();
+            AdamX.name="poya";
+            AdamX.temp=12344231 celcius;
+            AdamX.size="32942Km";
 
 
 
